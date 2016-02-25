9366e1ea0fb733fd3e020df816c91603eb83eda2
silas546 <silasowings@gmail.com>
2016-02-25 10:03:30 -0600

    shopify app generated

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.api_key = "e474a5d993d24ef520ae153e426b92df"
+  config.secret = "096f7fa63d53cf3ff992634880eee642"
+  config.redirect_uri = "<redirect_uri>"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
