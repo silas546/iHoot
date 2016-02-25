e5771c16b17c7e8e9535f199a76fed327513c894
silas546 <silasowings@gmail.com>
2016-02-25 15:56:35 -0600

    orders bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
-    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10})
+    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10, :order => "created_at DESC"})
   end
 end
