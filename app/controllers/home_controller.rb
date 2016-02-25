423184c6b54e1ddef92063091d57168ff42f9a01
silas546 <silasowings@gmail.com>
2016-02-25 16:21:52 -0600

    more detailed orders table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
-    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10, :order => "created_at DESC"})
+    
+    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10})
   end
 end
